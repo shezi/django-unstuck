9a98d87c76cc8d85948da5d37925e0ee4b00256a
Johannes Spielmann <j@spielmannsolutions.com>
2021-06-01 15:31:31 +0200

    added base template and vendored foundation

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "unstuck.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
